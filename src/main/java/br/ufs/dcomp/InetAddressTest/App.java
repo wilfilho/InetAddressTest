4d6399e155b33db79ebd9feab30aaea298492a8e
Ubuntu <ubuntu@ip-172-31-70-159.ec2.internal>
2022-02-11 23:30:39 +0000

    feat: changing to youtube host

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -10,9 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        try{
+        try {
             
-            String name = "www.uol.com.br";
+            String name = "www.youtube.com.br";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address.getHostAddress());
